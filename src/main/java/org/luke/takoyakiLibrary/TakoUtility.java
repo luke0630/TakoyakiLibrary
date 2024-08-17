cc62ad7a9ac2ca0b8453adcc82c3f47edba44d39
luke0630 <ryuto-0915@outlook.jp>
2024-08-17 14:16:47 +0900

    add: add getItem method.

--- src/main/java/org/luke/takoyakiLibrary/TakoUtility.java
+++ src/main/java/org/luke/takoyakiLibrary/TakoUtility.java
@@ -2,10 +2,21 @@ package org.luke.takoyakiLibrary;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public final class TakoUtility {
 
+    private TakoUtility() {}
+
+    public static ItemStack getItem(Material material, String name) {
+        var item = new ItemStack(material);
+        var meta = item.getItemMeta();
+        meta.setDisplayName(name);
+        item.setItemMeta(meta);
+        return item;
+    }
 
     public static String toColor(String message) {
         return ChatColor.translateAlternateColorCodes('&', message);
