52fe4e22af309beab19019a6bc915fb63e8a7451
luke0630 <ryuto-0915@outlook.jp>
2024-08-17 12:50:23 +0900

    add: add getInitInventory method.

--- src/main/java/org/luke/takoyakiLibrary/TakoUtility.java
+++ src/main/java/org/luke/takoyakiLibrary/TakoUtility.java
@@ -1,9 +1,18 @@
 package org.luke.takoyakiLibrary;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.inventory.Inventory;
 
 public final class TakoUtility {
+
+
     public static String toColor(String message) {
         return ChatColor.translateAlternateColorCodes('&', message);
     }
+
+    public static Inventory getInitInventory(String title, Integer size) {
+        return Bukkit.createInventory(null, size, toColor(title));
+    }
+
 }
